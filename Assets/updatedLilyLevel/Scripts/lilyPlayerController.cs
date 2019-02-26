2acbc765bf8a7fc6cd50cd5cdc08ce95ca1bc6be
scrublord12 <30794246+scrublord12@users.noreply.github.com>
2019-02-26 02:15:19 -0600

    script changes
    pseudo code added for lvl menus

--- Assets/updatedLilyLevel/Scripts/lilyPlayerController.cs
+++ Assets/updatedLilyLevel/Scripts/lilyPlayerController.cs
@@ -36,6 +36,10 @@ public class lilyPlayerController : MonoBehaviour {
     public float dogModeSpeed, dogModeJumpForce;
     public float beeModeSpeed, beeModeJumpForce;
     public float normalSpeed, normalJumpForce;
+
+    public Transform lilyStartPosition;
+
+    public bool reset;
     void Start() {
 
         rb = GetComponent<Rigidbody2D>();
@@ -50,70 +54,87 @@ public class lilyPlayerController : MonoBehaviour {
     }
 
     void Update() {
+        if(alreadyDead){
+           //start death transition to score menu
+           //start lily death animation
 
-        rb.velocity = new Vector2(speed, rb.velocity.y);
+           //change the z coord so lily falls off the platform
 
-        //isGrounded = Physics2D.IsTouchingLayers(myCollider, groundLayer);
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+           //detach lily from the camera move controller
 
-        if ((Input.GetMouseButtonDown(0) && isGrounded || Input.GetKeyDown(KeyCode.Space) && isGrounded) && speed > 0) {
+           //make lily object invisible
 
-            rb.velocity = new Vector2(rb.velocity.x, jump_Force);
-            JumpSounds[Random.Range(0, JumpSounds.Length)].GetComponent<AudioSource>().Play();
+           //transition to score menu
 
-            stoppedJumping = false;
-        }
+        }else{
+            rb.velocity = new Vector2(speed, rb.velocity.y);
 
-        if (((Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) && !stoppedJumping) && speed > 0) {
+            //isGrounded = Physics2D.IsTouchingLayers(myCollider, groundLayer);
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-            if (jump_Time_Counter > 0) {
+            if ((Input.GetMouseButtonDown(0) && isGrounded || Input.GetKeyDown(KeyCode.Space) && isGrounded) && speed > 0) {
+
                 rb.velocity = new Vector2(rb.velocity.x, jump_Force);
-                jump_Time_Counter -= Time.deltaTime;
+                JumpSounds[Random.Range(0, JumpSounds.Length)].GetComponent<AudioSource>().Play();
+
+                stoppedJumping = false;
+            }
+
+            if (((Input.GetKey(KeyCode.Space) || Input.GetMouseButton(0)) && !stoppedJumping) && speed > 0) {
+
+                if (jump_Time_Counter > 0) {
+                    rb.velocity = new Vector2(rb.velocity.x, jump_Force);
+                    jump_Time_Counter -= Time.deltaTime;
+                }
+            }
+
+            if ((Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)) && speed > 0) {
+                jump_Time_Counter = 0;
+                stoppedJumping = true;
+            }
+
+            if (isGrounded) {
+                jump_Time_Counter = jump_Time;
+            }
+            speed = normalSpeed;
+            jump_Force = normalJumpForce;
+
+            //dogmode = true condition
+            if(transform.position.x > dogModeStart.transform.position.x){
+                dogMode = true;
+
+            }
+            
+            if(dogMode == true){
+                
+                //change animations to dog animations
+                myAnim.SetBool("dogMode", dogMode);
+                myAnim.SetBool("Grounded", isGrounded);
+                myAnim.SetFloat("Speed", rb.velocity.x);
+                myAnim.SetFloat("Vertical_Speed", rb.velocity.y);
+
+                //increase speed
+                speed = dogModeSpeed;
+
+                //increase jump Force
+                jump_Force = dogModeJumpForce;
+
+                transform.localScale = new Vector3(2, 2, 1);
+            }else{
+                transform.localScale = new Vector3(-2, 2, 1);
+                speed = normalSpeed;
+                jump_Force = normalJumpForce;
+                myAnim.SetBool("dogMode", dogMode);
+                myAnim.SetBool("Grounded", isGrounded);
+                myAnim.SetFloat("Speed", rb.velocity.x);
+                myAnim.SetFloat("Vertical_Speed", rb.velocity.y);
+
             }
         }
 
-        if ((Input.GetKeyUp(KeyCode.Space) || Input.GetMouseButtonUp(0)) && speed > 0) {
-            jump_Time_Counter = 0;
-            stoppedJumping = true;
-        }
+    }
 
-        if (isGrounded) {
-            jump_Time_Counter = jump_Time;
-        }
-         speed = normalSpeed;
-         jump_Force = normalJumpForce;
-
-        //dogmode = true condition
-        if(transform.position.x > dogModeStart.transform.position.x){
-            dogMode = true;
-
-        }
-        
-        if(dogMode == true){
-            
-            //change animations to dog animations
-            myAnim.SetBool("dogMode", dogMode);
-            myAnim.SetBool("Grounded", isGrounded);
-            myAnim.SetFloat("Speed", rb.velocity.x);
-            myAnim.SetFloat("Vertical_Speed", rb.velocity.y);
-
-            //increase speed
-            speed = dogModeSpeed;
-
-            //increase jump Force
-            jump_Force = dogModeJumpForce;
-
-            transform.localScale = new Vector3(2, 2, 1);
-        }else{
-            transform.localScale = new Vector3(-2, 2, 1);
-            speed = normalSpeed;
-            jump_Force = normalJumpForce;
-            myAnim.SetBool("dogMode", dogMode);
-            myAnim.SetBool("Grounded", isGrounded);
-            myAnim.SetFloat("Speed", rb.velocity.x);
-            myAnim.SetFloat("Vertical_Speed", rb.velocity.y);
-
-        }
+    public void resetLevel(){
 
     }
 }
